dc03c0a2b0b510b782696ecd7c6f4f3739c411c4
jhla4 <jhla456@kakao.com>
2024-03-02 19:28:22 +0900

    v1.0.1

--- lib/bbanto.dart
+++ lib/bbanto.dart
@@ -27,6 +27,64 @@ class MyCard extends StatelessWidget {
         title: Text('BBANTO'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
+        elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          //EdgeInsets.fromLTRB(left, top, right, bottom)
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.cyan,
+                width: 400,
+                height: 100,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.amber,
+                width: 400,
+                height: 100,
+              ),
+              Text('Hello'),
+              SizedBox(
+                height: 10,
+              ),
+              Text('Hello'),
+              Text('Hello'),
+              Container(
+                color: Colors.blue,
+                width: 400,
+                height: 100,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 400,
+                height: 100,
+              ),
+              Container(
+                color: Colors.red,
+                width: 400,
+                height: 100,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.pink,
+                width: 400,
+                height: 100,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
